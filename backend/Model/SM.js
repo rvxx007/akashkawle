79c590c5b83d4bd80eb7353d838cb816c922c89b
Akash Kawale <akashkawle0@gmail.com>
2024-09-28 19:52:31 +0530

    Delete backend directory

--- backend/Model/SM.js
+++
@@ -1,9 +0,0 @@
-import mangoose from 'mongoose';
-
-const SM_Schema = new mangoose.Schema({
-    title:String,
-    img: String,
-    description: String,
-})
-
- const SM = mangoose.model('SM', SM_Schema);
